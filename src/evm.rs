cbfcdd89aa845cac04e81242afb73ba33b039b62
Shady Khalifa <shekohex@gmail.com>
2021-06-28 22:22:05 +0200

    add evm contracts with abigen

---
+++ src/evm.rs
@@ -0,0 +1,52 @@
+use ethers::prelude::*;
+
+// Generate the type-safe contract bindings by providing the ABI
+// definition in human readable format
+abigen!(AnchorContract, "contracts/Anchor.json");
+
+pub use ethereum_types;
+pub use ethers;
+pub use AnchorContract;
+
+#[cfg(all(test, feature = "integration-tests"))]
+mod tests {
+    use std::convert::TryFrom;
+    use std::str::FromStr;
+    use std::sync::Arc;
+    use std::time::Duration;
+
+    use super::*;
+
+    type DynError = Box<dyn std::error::Error + Sync + Send + 'static>;
+
+    struct BeresheetNetwork;
+    impl BeresheetNetwork {
+        fn endpoint() -> &'static str {
+            "http://beresheet3.edgewa.re:9933"
+        }
+
+        fn contract_10_tedg_address() -> Address {
+            Address::from_str("0x5f771fc87F87DB48C9fB11aA228D833226580689")
+                .unwrap()
+        }
+    }
+
+    #[tokio::test]
+    async fn contract_deposit() -> Result<(), DynError> {
+        let provider =
+            Provider::<Http>::try_from(BeresheetNetwork::endpoint())?
+                .interval(Duration::from_millis(10u64));
+        // TODO(shekohex): handle wallets.
+        let client = Arc::new(provider);
+        let contract = AnchorContract::new(
+            BeresheetNetwork::contract_10_tedg_address(),
+            client.clone(),
+        );
+        // create a deposit call with 10tEDG
+        let deposit = contract.deposit([0; 32]).value(10);
+        let result = deposit.send().await?;
+        let inblock = dbg!(result).await?;
+        dbg!(inblock);
+        Ok(())
+    }
+}
