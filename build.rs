01266c7d6545f1b06a1845676e3ac19b0e3d98ae
Shady Khalifa <shekohex@gmail.com>
2021-07-28 18:11:18 +0200

    build script improvements

--- build.rs
+++ build.rs
@@ -5,6 +5,8 @@ use ethers::contract::Abigen;
 use tempfile::NamedTempFile;
 
 fn main() -> Result<(), Box<dyn Error>> {
+    // Tell Cargo that if the given file changes, to rerun this build script.
+    println!("cargo:rerun-if-changed=contracts/Anchor.json");
     let anchor_contract_file =
         std::fs::read_to_string("contracts/Anchor.json")?;
     let anchor_raw: serde_json::Value =
