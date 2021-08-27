2a5bca0798b5f90da3104b56305d42f6168f6856
Drew Stone <drewstone329@gmail.com>
2021-08-27 11:53:54 -0400

    add more contracts

---
+++ src/evm/contract/darkwebb/mod.rs
@@ -0,0 +1,9 @@
+pub mod WEBBanchor2;
+pub mod anchor2;
+pub mod bridge;
+pub mod governance_bravo_delegate;
+
+pub use anchor2::*;
+pub use bridge::*;
+pub use governance_bravo_delegate::*;
+pub use WEBBanchor2::*;
