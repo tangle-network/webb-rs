6b4f5050f4727a6eec80fadd20683540f2b840b9
Drew Stone <drewstone329@gmail.com>
2022-04-19 11:14:28 +0200

    Fmt, add pub mod

--- src/evm/contract/protocol_solidity/mod.rs
+++ src/evm/contract/protocol_solidity/mod.rs
@@ -2,8 +2,10 @@ pub mod anchor_handler;
 pub mod anchor_proxy;
 pub mod fixed_deposit_anchor;
 pub mod signature_bridge;
+pub mod variable_anchor;
 
 pub use anchor_handler::*;
 pub use anchor_proxy::*;
 pub use fixed_deposit_anchor::*;
 pub use signature_bridge::*;
+pub use variable_anchor::*;
