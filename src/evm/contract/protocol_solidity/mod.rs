8b0ada9781e6250df140fdae45f2063245fabf6e
Drew Stone <drewstone329@gmail.com>
2022-04-23 14:51:16 +0200

    Fmt

--- src/evm/contract/protocol_solidity/mod.rs
+++ src/evm/contract/protocol_solidity/mod.rs
@@ -1,19 +1,19 @@
 pub mod anchor_handler;
 pub mod anchor_proxy;
 pub mod fixed_deposit_anchor;
-pub mod signature_bridge;
-pub mod variable_anchor;
 pub mod governed_token_wrapper;
+pub mod signature_bridge;
 pub mod token_wrapper_handler;
 pub mod treasury;
 pub mod treasury_handler;
+pub mod variable_anchor;
 
 pub use anchor_handler::*;
 pub use anchor_proxy::*;
 pub use fixed_deposit_anchor::*;
-pub use signature_bridge::*;
-pub use variable_anchor::*;
 pub use governed_token_wrapper::*;
+pub use signature_bridge::*;
 pub use token_wrapper_handler::*;
 pub use treasury::*;
 pub use treasury_handler::*;
+pub use variable_anchor::*;
