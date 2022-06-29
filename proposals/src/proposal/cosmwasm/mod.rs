a015e09e981a35aa22faca2b54c8f559f9cf7467
guorong <dudurong009@gmail.com>
2022-06-29 18:52:50 +0800

    Get rid of unnecessary "cosmwasm-proposals"

--- proposals/src/proposal/cosmwasm/mod.rs
+++ proposals/src/proposal/cosmwasm/mod.rs
@@ -6,19 +6,13 @@
 //! - [x] `MinWithdrawalLimitProposal`
 //! - [x] `MaxDepositLimitProposal`
 //! - [x] `ResourceIdUpdateProposal`
-//! - [x] `SetTreasuryHandlerProposal`
-//! - [x] `SetVerifierProposal`
 //! - [x] `FeeRecipientUpdateProposal`
-//! - [x] `RescueTokensProposal`
 
 mod anchor_update;
 mod fee_recipient_update;
 mod max_deposit_limit;
 mod min_withdrawal_limit;
-mod rescue_tokens;
 mod resource_id_update;
-mod set_treasury_handler;
-mod set_verifier;
 mod token_add;
 mod token_remove;
 mod utils;
@@ -28,10 +22,7 @@ pub use anchor_update::*;
 pub use fee_recipient_update::*;
 pub use max_deposit_limit::*;
 pub use min_withdrawal_limit::*;
-pub use rescue_tokens::*;
 pub use resource_id_update::*;
-pub use set_treasury_handler::*;
-pub use set_verifier::*;
 pub use token_add::*;
 pub use token_remove::*;
 pub use utils::*;
