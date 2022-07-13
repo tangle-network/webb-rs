ca3ab4a4d4e0d361a263ddd121bd3398a6526358
guorong <dudurong009@gmail.com>
2022-07-14 03:44:05 +0800

    Implement the "min_withdrawal_limit" for "cosmwasm"

--- proposals/src/proposal/cosmwasm/min_withdrawal_limit.rs
+++ proposals/src/proposal/cosmwasm/min_withdrawal_limit.rs
@@ -1,19 +1,13 @@
 //! Minimum Withdrawal Limit Proposal.
 use crate::ProposalHeader;
+use cosmwasm_std::{from_slice, to_binary, Uint128};
+use schemars::JsonSchema;
+use serde::{Deserialize, Serialize};
 
 /// Minimum Withdrawal Limit Proposal.
 ///
 /// The [`MinWithdrawalLimitProposal`] updates the minimum withdrawal amount
 /// allowed on the variable anchor system.
-///
-/// The format of the proposal looks like:
-/// ```text
-/// ┌────────────────────┬────────────────────────┐
-/// │                    │                        │
-/// │ ProposalHeader 40B │ MinWithdrawalLimit 32B │
-/// │                    │                        │
-/// └────────────────────┴────────────────────────┘
-/// ```
 #[allow(clippy::module_name_repetitions)]
 #[derive(Debug, Copy, Clone, PartialEq, Eq, Hash)]
 pub struct MinWithdrawalLimitProposal {
@@ -22,9 +16,6 @@ pub struct MinWithdrawalLimitProposal {
 }
 
 impl MinWithdrawalLimitProposal {
-    /// Length of the proposal in bytes.
-    pub const LENGTH: usize = ProposalHeader::LENGTH + 32; // min_withdrawal_limit
-
     /// Creates a new min withdrawal limit proposal.
     #[must_use]
     pub const fn new(header: ProposalHeader, min_limit: [u8; 32]) -> Self {
@@ -48,47 +39,62 @@ impl MinWithdrawalLimitProposal {
 
     /// Get the proposal as a bytes
     #[must_use]
-    pub fn to_bytes(&self) -> [u8; Self::LENGTH] {
-        let mut bytes = [0u8; Self::LENGTH];
-        let f = 0usize;
-        let t = ProposalHeader::LENGTH;
-        bytes[f..t].copy_from_slice(&self.header.to_bytes());
-        let f = t;
-        let t = t + 32;
-        bytes[f..t].copy_from_slice(&self.min_withdrawal_limit);
+    pub fn to_bytes(&self) -> Vec<u8> {
+        let mut bytes = vec![];
+        bytes.extend_from_slice(&self.header.to_bytes());
+
+        let mut withdraw_limit_bytes = [0u8; 16];
+        withdraw_limit_bytes
+            .copy_from_slice(self.min_withdrawal_limit.split_at(16).1);
+        let message = to_binary(&ConfigureMinimalWithdrawalLimit {
+            minimal_withdrawal_amount: Uint128::from(u128::from_be_bytes(
+                withdraw_limit_bytes,
+            )),
+        })
+        .unwrap();
+        bytes.extend_from_slice(message.as_slice());
+
         bytes
     }
 
     /// Get the proposal as a bytes without copying.
     #[must_use]
-    pub fn into_bytes(self) -> [u8; Self::LENGTH] {
+    pub fn into_bytes(self) -> Vec<u8> {
         self.to_bytes()
     }
 }
 
-impl From<[u8; MinWithdrawalLimitProposal::LENGTH]>
-    for MinWithdrawalLimitProposal
-{
-    fn from(bytes: [u8; MinWithdrawalLimitProposal::LENGTH]) -> Self {
+impl From<Vec<u8>> for MinWithdrawalLimitProposal {
+    fn from(bytes: Vec<u8>) -> Self {
         let f = 0usize;
         let t = ProposalHeader::LENGTH;
         let mut header_bytes = [0u8; ProposalHeader::LENGTH];
         header_bytes.copy_from_slice(&bytes[f..t]);
         let header = ProposalHeader::from(header_bytes);
-        let f = t;
-        let t = t + 32;
+
         let mut min_withdrawal_limit = [0u8; 32];
-        min_withdrawal_limit.copy_from_slice(&bytes[f..t]);
+        let f = t;
+        let msg_bytes = &bytes[f..];
+        let decoded_msg: ConfigureMinimalWithdrawalLimit =
+            from_slice(&msg_bytes).unwrap();
+        min_withdrawal_limit[16..].copy_from_slice(
+            &decoded_msg.minimal_withdrawal_amount.u128().to_be_bytes(),
+        );
+
         Self::new(header, min_withdrawal_limit)
     }
 }
 
-impl From<MinWithdrawalLimitProposal>
-    for [u8; MinWithdrawalLimitProposal::LENGTH]
-{
+impl From<MinWithdrawalLimitProposal> for Vec<u8> {
     fn from(proposal: MinWithdrawalLimitProposal) -> Self {
         proposal.to_bytes()
     }
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Eq, JsonSchema)]
+#[serde(rename_all = "snake_case")]
+struct ConfigureMinimalWithdrawalLimit {
+    minimal_withdrawal_amount: Uint128,
 }
 
 #[cfg(test)]
@@ -107,19 +113,18 @@ mod tests {
         let target_chain = TypedChainId::Cosmos(4);
         let resource_id = ResourceId::new(target_system, target_chain);
         let function_signature =
-            FunctionSignature::new(hex_literal::hex!("cafebabe"));
+            FunctionSignature::new(hex_literal::hex!("00000000"));
         let nonce = Nonce::from(0x0001);
         let header =
             ProposalHeader::new(resource_id, function_signature, nonce);
         let min_withdrawal_limit = hex_literal::hex!(
-            "000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f"
+            "00000000000000000000000000000000101112131415161718191a1b1c1d1e1f"
         );
         let proposal =
             MinWithdrawalLimitProposal::new(header, min_withdrawal_limit);
         let bytes = proposal.to_bytes();
         let expected = hex_literal::hex!(
-            "000000000000aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa040000000004"
-            "cafebabe00000001000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f"
+            "000000000000aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa04000000000400000000000000017b226d696e696d616c5f7769746864726177616c5f616d6f756e74223a223231333536323833353734303736383931343933393438393639393739363835343435313531227d"
         );
         assert_eq!(bytes, expected);
     }
@@ -127,10 +132,9 @@ mod tests {
     #[test]
     fn decode() {
         let bytes = hex_literal::hex!(
-            "000000000000aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa040000000004"
-            "cafebabe00000001000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f"
+            "000000000000aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa04000000000400000000000000017b226d696e696d616c5f7769746864726177616c5f616d6f756e74223a223231333536323833353734303736383931343933393438393639393739363835343435313531227d"
         );
-        let proposal = MinWithdrawalLimitProposal::from(bytes);
+        let proposal = MinWithdrawalLimitProposal::from(bytes.to_vec());
         let header = proposal.header();
         let resource_id = header.resource_id();
         let target_system = resource_id.target_system();
@@ -147,13 +151,13 @@ mod tests {
         assert_eq!(target_chain, TypedChainId::Cosmos(4));
         assert_eq!(
             function_signature,
-            FunctionSignature::new(hex_literal::hex!("cafebabe"))
+            FunctionSignature::new(hex_literal::hex!("00000000"))
         );
         assert_eq!(nonce, Nonce::from(0x0001));
         assert_eq!(
             min_withdrawal_limit,
             hex_literal::hex!(
-                "000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f"
+                "00000000000000000000000000000000101112131415161718191a1b1c1d1e1f"
             )
         );
     }
