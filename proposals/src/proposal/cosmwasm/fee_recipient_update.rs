23acae856fe830ab09194323ceadb46e290c78fc
guorong <dudurong009@gmail.com>
2022-07-14 02:06:38 +0800

    Implement the "fee_recipient_update" proposal for 'cosmwasm'

--- proposals/src/proposal/cosmwasm/fee_recipient_update.rs
+++ proposals/src/proposal/cosmwasm/fee_recipient_update.rs
@@ -1,33 +1,24 @@
 //! Fee Recipient Update Proposal.
+use cosmwasm_std::{from_slice, to_binary, Uint128};
+use schemars::JsonSchema;
+use serde::{Deserialize, Serialize};
 use crate::ProposalHeader;
 
 /// Fee Recipient Update Proposal.
 ///
 /// The [`FeeRecipientUpdateProposal`] updates the recipient of the wrapping
 /// fees.
-///
-/// The format of the proposal looks like:
-/// ```text
-/// ┌────────────────────┬─────────────────────────┐
-/// │                    │                         │
-/// │ ProposalHeader 40B │ FeeRecipientAddress 20B │
-/// │                    │                         │
-/// └────────────────────┴─────────────────────────┘
-/// ```
 #[allow(clippy::module_name_repetitions)]
-#[derive(Debug, Copy, Clone, PartialEq, Eq, Hash)]
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
 pub struct FeeRecipientUpdateProposal {
     header: ProposalHeader,
-    fee_recipient_address: [u8; 20],
+    fee_recipient_address: String,
 }
 
 impl FeeRecipientUpdateProposal {
-    /// Length of the proposal in bytes.
-    pub const LENGTH: usize = ProposalHeader::LENGTH + 20; // fee_recipient_address
-
     /// Creates a new fee recipient update proposal.
     #[must_use]
-    pub const fn new(header: ProposalHeader, address: [u8; 20]) -> Self {
+    pub const fn new(header: ProposalHeader, address: String) -> Self {
         Self {
             header,
             fee_recipient_address: address,
@@ -42,53 +33,67 @@ impl FeeRecipientUpdateProposal {
 
     /// Get the fee recipient address.
     #[must_use]
-    pub const fn fee_recipient_address(&self) -> [u8; 20] {
-        self.fee_recipient_address
+    pub fn fee_recipient_address(&self) -> String {
+        self.fee_recipient_address.clone()
     }
 
     /// Get the proposal as a bytes
     #[must_use]
-    pub fn to_bytes(&self) -> [u8; Self::LENGTH] {
-        let mut bytes = [0u8; Self::LENGTH];
-        let f = 0usize;
-        let t = ProposalHeader::LENGTH;
-        bytes[f..t].copy_from_slice(&self.header.to_bytes());
-        let f = t;
-        let t = t + 20;
-        bytes[f..t].copy_from_slice(&self.fee_recipient_address);
+    pub fn to_bytes(&self) -> Vec<u8> {
+        let mut bytes = self.header.to_bytes().to_vec();
+
+        let message = to_binary(&UpdateConfigMsg {
+            governor: None,
+            is_native_allowed: None,
+            wrapping_limit: None,
+            fee_percentage: None,
+            fee_recipient: Some(self.fee_recipient_address()),
+        })
+        .unwrap();
+        bytes.extend_from_slice(&message.to_vec());
+
         bytes
     }
 
     /// Get the proposal as a bytes without copying.
     #[must_use]
-    pub fn into_bytes(self) -> [u8; Self::LENGTH] {
+    pub fn into_bytes(self) -> Vec<u8> {
         self.to_bytes()
     }
 }
 
-impl From<[u8; FeeRecipientUpdateProposal::LENGTH]>
-    for FeeRecipientUpdateProposal
+impl From<Vec<u8>> for FeeRecipientUpdateProposal
 {
-    fn from(bytes: [u8; FeeRecipientUpdateProposal::LENGTH]) -> Self {
+    fn from(bytes: Vec<u8>) -> Self {
         let f = 0usize;
         let t = ProposalHeader::LENGTH;
         let mut header_bytes = [0u8; ProposalHeader::LENGTH];
         header_bytes.copy_from_slice(&bytes[f..t]);
         let header = ProposalHeader::from(header_bytes);
+        
         let f = t;
-        let t = t + 20;
-        let mut address = [0u8; 20];
-        address.copy_from_slice(&bytes[f..t]);
-        Self::new(header, address)
+        let msg_bytes = &bytes[f..];
+        let decoded_msg: UpdateConfigMsg = from_slice(&msg_bytes).unwrap();
+
+        Self::new(header, decoded_msg.fee_recipient.unwrap_or("".to_string()))
     }
 }
 
-impl From<FeeRecipientUpdateProposal>
-    for [u8; FeeRecipientUpdateProposal::LENGTH]
+impl From<FeeRecipientUpdateProposal> for Vec<u8>
 {
     fn from(proposal: FeeRecipientUpdateProposal) -> Self {
         proposal.to_bytes()
     }
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Eq, JsonSchema)]
+#[serde(rename_all = "snake_case")]
+struct UpdateConfigMsg {
+    pub governor: Option<String>,
+    pub is_native_allowed: Option<bool>,
+    pub wrapping_limit: Option<Uint128>,
+    pub fee_percentage: Option<u8>,
+    pub fee_recipient: Option<String>,
 }
 
 #[cfg(test)]
@@ -107,29 +112,24 @@ mod tests {
         let target_chain = TypedChainId::Cosmos(4);
         let resource_id = ResourceId::new(target_system, target_chain);
         let function_signature =
-            FunctionSignature::new(hex_literal::hex!("cafebabe"));
+            FunctionSignature::new(hex_literal::hex!("00000000"));
         let nonce = Nonce::from(0x0001);
         let header =
             ProposalHeader::new(resource_id, function_signature, nonce);
-        let new_fee_recipient_address =
-            hex_literal::hex!("bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb");
+        let new_fee_recipient_address = "juno14hj2tavq8fpesdwxxcu44rty3hh90vhujrvcmstl4zr3txmfvw9skjuwg8".to_string();
         let proposal =
             FeeRecipientUpdateProposal::new(header, new_fee_recipient_address);
         let bytes = proposal.to_bytes();
         let expected = hex_literal::hex!(
-            "000000000000aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa040000000004"
-            "cafebabe00000001bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb"
+            "000000000000aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa04000000000400000000000000017b22676f7665726e6f72223a6e756c6c2c2269735f6e61746976655f616c6c6f776564223a6e756c6c2c227772617070696e675f6c696d6974223a6e756c6c2c226665655f70657263656e74616765223a6e756c6c2c226665655f726563697069656e74223a226a756e6f3134686a32746176713866706573647778786375343472747933686839307668756a7276636d73746c347a723374786d66767739736b6a75776738227d"
         );
         assert_eq!(bytes, expected);
     }
 
     #[test]
     fn decode() {
-        let bytes = hex_literal::hex!(
-            "000000000000aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa040000000004"
-            "cafebabe00000001bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb"
-        );
-        let proposal = FeeRecipientUpdateProposal::from(bytes);
+        let bytes = hex_literal::hex!("000000000000aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa04000000000400000000000000017b22676f7665726e6f72223a6e756c6c2c2269735f6e61746976655f616c6c6f776564223a6e756c6c2c227772617070696e675f6c696d6974223a6e756c6c2c226665655f70657263656e74616765223a6e756c6c2c226665655f726563697069656e74223a226a756e6f3134686a32746176713866706573647778786375343472747933686839307668756a7276636d73746c347a723374786d66767739736b6a75776738227d");
+        let proposal = FeeRecipientUpdateProposal::from(bytes.to_vec());
         let header = proposal.header();
         let resource_id = header.resource_id();
         let target_system = resource_id.target_system();
@@ -145,12 +145,12 @@ mod tests {
         assert_eq!(target_chain, TypedChainId::Cosmos(4));
         assert_eq!(
             function_signature,
-            FunctionSignature::new(hex_literal::hex!("cafebabe"))
+            FunctionSignature::new(hex_literal::hex!("00000000"))
         );
         assert_eq!(nonce, Nonce::from(0x0001));
         assert_eq!(
             proposal.fee_recipient_address(),
-            hex_literal::hex!("bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb")
+            "juno14hj2tavq8fpesdwxxcu44rty3hh90vhujrvcmstl4zr3txmfvw9skjuwg8".to_string(),
         );
     }
 }
