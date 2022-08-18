7b00e70f61ec9ffdfab2ebb6b0a9497a30bed82e
Drew Stone <drewstone329@gmail.com>
2022-08-18 16:23:14 -0400

    Make little script in test

--- bridge-proofs/src/lib.rs
+++ bridge-proofs/src/lib.rs
@@ -1,8 +1,28 @@
 #[cfg(test)]
 mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+    use ethers::prelude::*;
+
+    #[tokio::test]
+    async fn get_proof_works() {
+        let provider =
+            Provider::<Http>::try_from("http://127.0.0.1:5001").unwrap();
+
+        let block = provider.get_block(1u64).await.unwrap();
+        println!("Got block: {}", serde_json::to_string(&block).unwrap());
+
+        let address_vec =
+            hex::decode("bfce6B877Ebff977bB6e80B24FbBb7bC4eBcA4df").unwrap();
+        let mut address_bytes = [0u8; 20];
+        address_bytes.copy_from_slice(&address_vec);
+        println!("Got address: {:?}, {:?}", address_bytes, address_vec.len());
+        let proof = provider
+            .get_proof(
+                NameOrAddress::Address(address_bytes.into()),
+                vec![],
+                None,
+            )
+            .await
+            .unwrap();
+        println!("Got proof: {}", serde_json::to_string(&proof).unwrap());
     }
 }
