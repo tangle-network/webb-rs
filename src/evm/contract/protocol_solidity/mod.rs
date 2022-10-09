b3b4f275a59c033ba342ee896b7d4da4d66bb842
Drew Stone <drewstone329@gmail.com>
2022-10-09 06:19:01 -0400

    Export the new modules

--- src/evm/contract/protocol_solidity/mod.rs
+++ src/evm/contract/protocol_solidity/mod.rs
@@ -1,19 +1,25 @@
 pub mod anchor_base;
 pub mod anchor_handler;
 pub mod governed_token_wrapper;
+pub mod open_vanchor_base;
+pub mod open_variable_anchor;
 pub mod signature_bridge;
 pub mod token_wrapper;
 pub mod token_wrapper_handler;
 pub mod treasury;
 pub mod treasury_handler;
+pub mod vanchor_base;
 pub mod variable_anchor;
 
 pub use anchor_base::*;
 pub use anchor_handler::*;
 pub use governed_token_wrapper::*;
+pub use open_vanchor_base::*;
+pub use open_variable_anchor::*;
 pub use signature_bridge::*;
 pub use token_wrapper::*;
 pub use token_wrapper_handler::*;
 pub use treasury::*;
 pub use treasury_handler::*;
+pub use vanchor_base::*;
 pub use variable_anchor::*;
