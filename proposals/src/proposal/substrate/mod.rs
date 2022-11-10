77ccd428f8f8e890e7c94dce4f8291d3726a6cb0
Salman Pathan <pathansalman555@gmail.com>
2022-11-11 03:53:26 +0530

    substrate proposals (#99)
    Co-authored-by: Dustin Brickwood <dustinbrickwood204@gmail.com>
    Co-authored-by: drewstone <drewstone329@gmail.com>

--- proposals/src/proposal/substrate/mod.rs
+++ proposals/src/proposal/substrate/mod.rs
@@ -4,14 +4,26 @@
 //! - [x] `TokenRemoveProposal`
 //! - [x] `WrappingFeeUpdateProposal`
 //! - [x] `ResourceIdUpdateProposal`
+//! - [x] `FeeRecipientUpdateProposal`
+//! - [x] `MaxDepositLimitProposal`
+//! - [x] `MinWithdrawalLimitProposal`
+//! - [x] `RescueTokensProposal`
 
 mod anchor_update;
+mod fee_recipient_update;
+mod max_deposit_limit;
+mod min_withdrawal_limit;
+mod rescue_tokens;
 mod resource_id_update;
 mod token_add;
 mod token_remove;
 mod wrapping_fee_update;
 
 pub use anchor_update::*;
+pub use fee_recipient_update::*;
+pub use max_deposit_limit::*;
+pub use min_withdrawal_limit::*;
+pub use rescue_tokens::*;
 pub use resource_id_update::*;
 pub use token_add::*;
 pub use token_remove::*;
