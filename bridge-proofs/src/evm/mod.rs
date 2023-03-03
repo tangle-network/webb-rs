8048030bd4ab3da9fbd667310c1a05dcd506cf08
Akilesh Tangella <saiakilesh@gmail.com>
2023-03-03 13:13:33 -0500

    light client proofs progress (still not working)

--- bridge-proofs/src/evm/mod.rs
+++ bridge-proofs/src/evm/mod.rs
@@ -1,63 +1,63 @@
-//! EVM Bridge Proofs
-#![cfg_attr(not(feature = "std"), no_std)]
+// //! EVM Bridge Proofs
+// #![cfg_attr(not(feature = "std"), no_std)]
 
-#[cfg(feature = "std")]
-use serde::{Deserialize, Serialize};
+// #[cfg(feature = "std")]
+// use serde::{Deserialize, Serialize};
 
-use ethereum_types::{Address, H256, U256, U64};
-use scale_codec::{Decode, Encode};
+// use ethereum_types::{Address, H256, U256, U64};
+// use scale_codec::{Decode, Encode};
 
-#[cfg(test)]
-mod tests;
+// #[cfg(test)]
+// mod tests;
 
-mod verify_trie_proof;
+pub mod verify_trie_proof;
 
-mod types;
+// mod types;
 
-use webb_proposals::{
-    evm::AnchorUpdateProposal, FunctionSignature, Nonce, ProposalHeader,
-    ResourceId, TargetSystem, TypedChainId,
-};
+// use webb_proposals::{
+//     evm::AnchorUpdateProposal, FunctionSignature, Nonce, ProposalHeader,
+//     ResourceId, TargetSystem, TypedChainId,
+// };
 
-#[derive(
-    Debug, Default, Clone, PartialEq, Eq, Encode, Decode, scale_info::TypeInfo,
-)]
-#[cfg_attr(feature = "std", derive(Serialize, Deserialize))]
-pub struct StorageProof {
-    pub key: H256,
-    pub proof: Vec<Vec<u8>>,
-    pub value: U256,
-}
+// #[derive(
+//     Debug, Default, Clone, PartialEq, Eq, Encode, Decode, scale_info::TypeInfo,
+// )]
+// #[cfg_attr(feature = "std", derive(Serialize, Deserialize))]
+// pub struct StorageProof {
+//     pub key: H256,
+//     pub proof: Vec<Vec<u8>>,
+//     pub value: U256,
+// }
 
-#[derive(Debug, Default, Clone, PartialEq, Eq, Encode, Decode)]
-#[cfg_attr(feature = "std", derive(Serialize, Deserialize))]
-pub struct EIP1186Proof {
-    pub address: Address,
-    pub balance: U256,
-    pub code_hash: H256,
-    pub nonce: U64,
-    pub storage_hash: H256,
-    pub account_proof: Vec<Vec<u8>>,
-    pub storage_proof: Vec<StorageProof>,
-}
+// #[derive(Debug, Default, Clone, PartialEq, Eq, Encode, Decode)]
+// #[cfg_attr(feature = "std", derive(Serialize, Deserialize))]
+// pub struct EIP1186Proof {
+//     pub address: Address,
+//     pub balance: U256,
+//     pub code_hash: H256,
+//     pub nonce: U64,
+//     pub storage_hash: H256,
+//     pub account_proof: Vec<Vec<u8>>,
+//     pub storage_proof: Vec<StorageProof>,
+// }
 
-pub fn get_anchor_update_proposal(
-    linked_resource_ids: Vec<ResourceId>,
-    log_index: u64,
-    log_entry_data: Vec<u8>,
-    receipt_index: u64,
-    receipt_data: Vec<u8>,
-    header_data: Vec<u8>,
-    proof: Vec<Vec<u8>>,
-) -> AnchorUpdateProposal {
-    let src_chain = TypedChainId::Evm(1);
-    let src_target_system = TargetSystem::ContractAddress([0u8; 20]);
-    let src_resource_id = ResourceId::new(src_target_system, src_chain);
-    let header = ProposalHeader::new(
-        src_resource_id,
-        FunctionSignature::new([0u8; 4]),
-        Nonce(1u32),
-    );
-    let merkle_root = [0u8; 32];
-    AnchorUpdateProposal::new(header, merkle_root, src_resource_id)
-}
+// pub fn get_anchor_update_proposal(
+//     linked_resource_ids: Vec<ResourceId>,
+//     log_index: u64,
+//     log_entry_data: Vec<u8>,
+//     receipt_index: u64,
+//     receipt_data: Vec<u8>,
+//     header_data: Vec<u8>,
+//     proof: Vec<Vec<u8>>,
+// ) -> AnchorUpdateProposal {
+//     let src_chain = TypedChainId::Evm(1);
+//     let src_target_system = TargetSystem::ContractAddress([0u8; 20]);
+//     let src_resource_id = ResourceId::new(src_target_system, src_chain);
+//     let header = ProposalHeader::new(
+//         src_resource_id,
+//         FunctionSignature::new([0u8; 4]),
+//         Nonce(1u32),
+//     );
+//     let merkle_root = [0u8; 32];
+//     AnchorUpdateProposal::new(header, merkle_root, src_resource_id)
+// }
--- bridge-proofs/src/lib.rs
+++ bridge-proofs/src/lib.rs
@@ -1,51 +1,31 @@
 mod evm;
 
+
 #[cfg(test)]
 mod tests {
     use ethers::prelude::*;
     use ethers::types::ValueOrArray::{Array, Value};
+    use ethers_core::k256::elliptic_curve::rand_core::block;
+    use crate::evm::verify_trie_proof::*;
+    use ethereum_types::H256 as EthTypeH256;
 
     #[tokio::test]
     async fn get_proof_works() {
         let provider =
-            Provider::<Http>::try_from("http://127.0.0.1:5001").unwrap();
-
-        let block = provider.get_block(1u64).await.unwrap();
-        println!("Got block: {}", serde_json::to_string(&block).unwrap());
+            Provider::<Http>::try_from("https://eth-mainnet.g.alchemy.com/v2/gWJbkK4UtKn4wd21HFa59KRIdNk_xs3q").unwrap();
 
         let address_vec =
-            hex::decode("bfce6B877Ebff977bB6e80B24FbBb7bC4eBcA4df").unwrap();
+            hex::decode("6fC21092DA55B392b045eD78F4732bff3C580e2c".to_lowercase()).unwrap();
         let mut address_bytes = [0u8; 20];
         address_bytes.copy_from_slice(&address_vec);
         println!("Got address: {:?}, {:?}", address_bytes, address_vec.len());
+
         let slot = hex::decode(
-            "ac33ff75c19e70fe83507db0d683fd3465c996598dc972688b7ace676c89077b",
-        )
-        .unwrap();
-
-        let block_number = provider.get_block_number().await.unwrap();
-
-        let block = provider.get_block(H256::from_slice(hex::decode("b24389717b3da1bb292d2e91a11f35278164a27868ffb0899388eeab20f01c2c").unwrap().as_slice())).await.unwrap();
-        println!("Got block: {}", serde_json::to_string(&block).unwrap());
-
-        let tx = provider.get_transaction_receipt(H256::from_slice(hex::decode("352019e5b0b1f6b9a325200db3e65c902381a4c0172081ec35a3499000152c81").unwrap().as_slice())).await.unwrap();
-        println!("Got tx: {}", serde_json::to_string(&tx).unwrap());
-
-        let logs = provider
-            .get_logs(&Filter {
-                block_option: FilterBlockOption::Range {
-                    from_block: Some(BlockNumber::Earliest),
-                    to_block: Some(BlockNumber::Latest),
-                },
-                address: Some(Value(H160::from_slice(&address_bytes))),
-                topics: [None, None, None, None],
-            })
-            .await
-            .unwrap();
-        println!("Got logs: {}", serde_json::to_string(&logs).unwrap());
-
+            "0000000000000000000000000000000000000000000000000000000000000000",
+        );
         let mut slot_bytes = [0u8; 32];
-        slot_bytes.copy_from_slice(&slot);
+        slot_bytes.copy_from_slice(&slot.unwrap());
+        
         let proof = provider
             .get_proof(
                 NameOrAddress::Address(address_bytes.into()),
@@ -55,5 +35,16 @@ mod tests {
             .await
             .unwrap();
         println!("Got proof: {}", serde_json::to_string(&proof).unwrap());
+
+        let storage_hash = (&proof).storage_hash;
+
+        let first_storage_proof = &(&proof).storage_proof[0].proof;
+        let first_storage_key = &(&proof).storage_proof[0].key;
+
+        TrieProver::verify_trie_proof(
+            EthTypeH256::from(storage_hash.as_fixed_bytes()),
+            H256::to_fixed_bytes(first_storage_key.clone()).to_vec(),
+            first_storage_proof.iter().map(|x| x.to_vec()).collect::<Vec<Vec<u8>>>(),
+        );
     }
 }
