23a3dec1c755e01847c1ff44121b227ee2101efb
1xstj <106580853+1xstj@users.noreply.github.com>
2023-03-07 12:54:05 +0400

    add bounds to nonce

--- proposals/src/traits.rs
+++ proposals/src/traits.rs
@@ -1,14 +1,16 @@
 use crate::Proposal;
+#[cfg(feature = "substrate")]
+use frame_support::{pallet_prelude::Get, BoundedVec};
 
 /// Trait to be used for handling signed proposals
-pub trait OnSignedProposal<E> {
+pub trait OnSignedProposal<E, MaxLength: Get<u32>> {
     /// On a signed proposal, this method is called.
     /// It returns a result `()` and otherwise an error of type `E`.
-    fn on_signed_proposal(proposal: Proposal) -> Result<(), E>;
+    fn on_signed_proposal(proposal: Proposal<MaxLength>) -> Result<(), E>;
 }
 
-impl<E> OnSignedProposal<E> for () {
-    fn on_signed_proposal(_: Proposal) -> Result<(), E> {
+impl<E, MaxLength: Get<u32>> OnSignedProposal<E, MaxLength> for () {
+    fn on_signed_proposal(_: Proposal<MaxLength>) -> Result<(), E> {
         Ok(())
     }
 }
