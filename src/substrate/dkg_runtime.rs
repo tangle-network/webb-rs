2bbe7b6a4f92e18de39572b3b1f2a8e3e6ee57f4
Nutomic <me@nutomic.com>
2023-03-14 16:16:30 +0100

    Update dkg-substrate metadata (#124)

--- src/substrate/dkg_runtime.rs
+++ src/substrate/dkg_runtime.rs
@@ -4955,11 +4955,11 @@ pub mod api {
                             call: ::std::boxed::Box::new(call),
                         },
                         [
-                            191u8, 184u8, 7u8, 103u8, 138u8, 68u8, 7u8, 75u8,
-                            112u8, 130u8, 244u8, 180u8, 164u8, 252u8, 107u8,
-                            175u8, 43u8, 3u8, 137u8, 71u8, 113u8, 114u8, 125u8,
-                            66u8, 193u8, 71u8, 143u8, 143u8, 52u8, 182u8,
-                            232u8, 249u8,
+                            145u8, 174u8, 191u8, 11u8, 91u8, 237u8, 161u8,
+                            70u8, 74u8, 157u8, 163u8, 181u8, 195u8, 83u8,
+                            127u8, 252u8, 23u8, 220u8, 172u8, 203u8, 102u8,
+                            126u8, 103u8, 130u8, 142u8, 147u8, 192u8, 58u8,
+                            181u8, 33u8, 255u8, 217u8,
                         ],
                     )
                 }
@@ -4987,11 +4987,11 @@ pub mod api {
                             weight,
                         },
                         [
-                            214u8, 135u8, 7u8, 122u8, 139u8, 217u8, 110u8,
-                            66u8, 158u8, 61u8, 18u8, 115u8, 167u8, 216u8, 85u8,
-                            106u8, 144u8, 190u8, 204u8, 229u8, 71u8, 209u8,
-                            187u8, 88u8, 36u8, 241u8, 110u8, 127u8, 240u8,
-                            174u8, 220u8, 68u8,
+                            103u8, 21u8, 24u8, 228u8, 231u8, 1u8, 104u8, 145u8,
+                            5u8, 255u8, 94u8, 139u8, 54u8, 17u8, 8u8, 32u8,
+                            37u8, 17u8, 212u8, 154u8, 40u8, 249u8, 79u8, 234u8,
+                            10u8, 72u8, 141u8, 172u8, 197u8, 238u8, 206u8,
+                            73u8,
                         ],
                     )
                 }
@@ -5052,11 +5052,11 @@ pub mod api {
                             call: ::std::boxed::Box::new(call),
                         },
                         [
-                            219u8, 115u8, 212u8, 30u8, 192u8, 29u8, 155u8,
-                            139u8, 64u8, 254u8, 125u8, 38u8, 137u8, 217u8,
-                            93u8, 167u8, 214u8, 220u8, 60u8, 131u8, 27u8,
-                            162u8, 19u8, 176u8, 53u8, 64u8, 135u8, 229u8,
-                            188u8, 250u8, 35u8, 78u8,
+                            175u8, 78u8, 218u8, 212u8, 82u8, 92u8, 87u8, 243u8,
+                            81u8, 241u8, 37u8, 72u8, 121u8, 144u8, 199u8, 44u8,
+                            56u8, 83u8, 233u8, 245u8, 128u8, 77u8, 244u8,
+                            154u8, 214u8, 176u8, 24u8, 157u8, 67u8, 43u8, 30u8,
+                            136u8,
                         ],
                     )
                 }
@@ -10177,11 +10177,11 @@ pub mod api {
                         "set_metadata",
                         SetMetadata { bridge_index, info },
                         [
-                            61u8, 127u8, 197u8, 96u8, 192u8, 145u8, 189u8,
-                            42u8, 76u8, 101u8, 10u8, 85u8, 74u8, 15u8, 78u8,
-                            133u8, 69u8, 145u8, 232u8, 46u8, 231u8, 201u8,
-                            135u8, 143u8, 11u8, 181u8, 176u8, 40u8, 117u8,
-                            87u8, 11u8, 87u8,
+                            72u8, 84u8, 159u8, 244u8, 136u8, 19u8, 190u8,
+                            187u8, 185u8, 86u8, 180u8, 197u8, 241u8, 97u8,
+                            235u8, 39u8, 114u8, 90u8, 112u8, 70u8, 93u8, 230u8,
+                            236u8, 25u8, 109u8, 152u8, 155u8, 107u8, 160u8,
+                            183u8, 137u8, 242u8,
                         ],
                     )
                 }
@@ -10235,7 +10235,7 @@ pub mod api {
                     )
                 }
                 #[doc = " Details of the module's parameters"]                pub fn bridges (& self , _0 : impl :: std :: borrow :: Borrow < :: core :: primitive :: u32 > ,) -> :: subxt :: storage :: address :: StaticStorageAddress :: < :: subxt :: metadata :: DecodeStaticType < runtime_types :: pallet_bridge_registry :: types :: BridgeMetadata > , :: subxt :: storage :: address :: Yes , () , :: subxt :: storage :: address :: Yes >{
-                    :: subxt :: storage :: address :: StaticStorageAddress :: new ("BridgeRegistry" , "Bridges" , vec ! [:: subxt :: storage :: address :: StorageMapKey :: new (_0 . borrow () , :: subxt :: storage :: address :: StorageHasher :: Blake2_256)] , [224u8 , 222u8 , 82u8 , 52u8 , 223u8 , 254u8 , 145u8 , 5u8 , 238u8 , 195u8 , 156u8 , 210u8 , 23u8 , 136u8 , 25u8 , 156u8 , 225u8 , 95u8 , 119u8 , 183u8 , 51u8 , 9u8 , 19u8 , 73u8 , 60u8 , 61u8 , 59u8 , 235u8 , 44u8 , 98u8 , 118u8 , 252u8 ,])
+                    :: subxt :: storage :: address :: StaticStorageAddress :: new ("BridgeRegistry" , "Bridges" , vec ! [:: subxt :: storage :: address :: StorageMapKey :: new (_0 . borrow () , :: subxt :: storage :: address :: StorageHasher :: Blake2_256)] , [1u8 , 112u8 , 75u8 , 147u8 , 69u8 , 209u8 , 218u8 , 149u8 , 213u8 , 112u8 , 94u8 , 202u8 , 240u8 , 100u8 , 216u8 , 77u8 , 14u8 , 111u8 , 54u8 , 86u8 , 79u8 , 109u8 , 31u8 , 34u8 , 31u8 , 24u8 , 30u8 , 64u8 , 29u8 , 29u8 , 64u8 , 3u8 ,])
                 }
                 #[doc = " Details of the module's parameters"]                pub fn bridges_root (& self ,) -> :: subxt :: storage :: address :: StaticStorageAddress :: < :: subxt :: metadata :: DecodeStaticType < runtime_types :: pallet_bridge_registry :: types :: BridgeMetadata > , () , () , :: subxt :: storage :: address :: Yes >{
                     ::subxt::storage::address::StaticStorageAddress::new(
@@ -10243,11 +10243,11 @@ pub mod api {
                         "Bridges",
                         Vec::new(),
                         [
-                            224u8, 222u8, 82u8, 52u8, 223u8, 254u8, 145u8, 5u8,
-                            238u8, 195u8, 156u8, 210u8, 23u8, 136u8, 25u8,
-                            156u8, 225u8, 95u8, 119u8, 183u8, 51u8, 9u8, 19u8,
-                            73u8, 60u8, 61u8, 59u8, 235u8, 44u8, 98u8, 118u8,
-                            252u8,
+                            1u8, 112u8, 75u8, 147u8, 69u8, 209u8, 218u8, 149u8,
+                            213u8, 112u8, 94u8, 202u8, 240u8, 100u8, 216u8,
+                            77u8, 14u8, 111u8, 54u8, 86u8, 79u8, 109u8, 31u8,
+                            34u8, 31u8, 24u8, 30u8, 64u8, 29u8, 29u8, 64u8,
+                            3u8,
                         ],
                     )
                 }
@@ -12948,7 +12948,7 @@ pub mod api {
                     Eq,
                     PartialEq,
                 )]
-                pub struct BridgeInfo { pub additional : runtime_types :: sp_core :: bounded :: bounded_vec :: BoundedVec < (runtime_types :: pallet_identity :: types :: Data , runtime_types :: pallet_identity :: types :: Data ,) > , pub display : runtime_types :: pallet_identity :: types :: Data , }
+                pub struct BridgeInfo { pub additional : runtime_types :: sp_core :: bounded :: bounded_vec :: BoundedVec < (runtime_types :: pallet_bridge_registry :: types :: SerdeData , runtime_types :: pallet_bridge_registry :: types :: SerdeData ,) > , pub display : runtime_types :: pallet_bridge_registry :: types :: SerdeData , }
                 #[derive(
                     :: subxt :: ext :: codec :: Decode,
                     :: subxt :: ext :: codec :: Encode,
@@ -12958,6 +12958,17 @@ pub mod api {
                     PartialEq,
                 )]
                 pub struct BridgeMetadata { pub resource_ids : runtime_types :: sp_core :: bounded :: bounded_vec :: BoundedVec < runtime_types :: webb_proposals :: header :: ResourceId > , pub info : runtime_types :: pallet_bridge_registry :: types :: BridgeInfo , }
+                #[derive(
+                    :: subxt :: ext :: codec :: Decode,
+                    :: subxt :: ext :: codec :: Encode,
+                    Clone,
+                    Debug,
+                    Eq,
+                    PartialEq,
+                )]
+                pub struct SerdeData(
+                    pub runtime_types::pallet_identity::types::Data,
+                );
             }
         }
         pub mod pallet_dkg_metadata {
@@ -17010,10 +17021,10 @@ pub mod api {
         let runtime_metadata_hash = client.metadata().metadata_hash(&PALLETS);
         if runtime_metadata_hash
             != [
-                103u8, 202u8, 180u8, 96u8, 131u8, 132u8, 13u8, 9u8, 94u8,
-                144u8, 119u8, 173u8, 198u8, 34u8, 227u8, 202u8, 26u8, 175u8,
-                118u8, 210u8, 82u8, 161u8, 33u8, 141u8, 186u8, 74u8, 82u8, 2u8,
-                172u8, 14u8, 250u8, 182u8,
+                254u8, 104u8, 199u8, 208u8, 36u8, 17u8, 130u8, 13u8, 109u8,
+                62u8, 139u8, 217u8, 72u8, 116u8, 75u8, 133u8, 84u8, 46u8,
+                140u8, 126u8, 191u8, 10u8, 135u8, 210u8, 211u8, 24u8, 36u8,
+                37u8, 203u8, 87u8, 211u8, 190u8,
             ]
         {
             Err(::subxt::error::MetadataError::IncompatibleMetadata)
