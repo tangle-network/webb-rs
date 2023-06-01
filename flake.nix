92649ae84fcb8c58354477e33f2100007e713a84
Shady Khalifa <dev+github@shadykhalifa.me>
2023-06-01 15:48:53 +0300

    Add nix flake devenv

---
+++ flake.nix
@@ -0,0 +1,50 @@
+{
+  description = "Webb SDK development environment";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    # Rust
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-utils.follows = "flake-utils";
+      };
+    };
+  };
+
+  outputs = { self, nixpkgs, rust-overlay, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+        lib = pkgs.lib;
+        toolchain = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          name = "webb-rs";
+          nativeBuildInputs = [
+            pkgs.pkg-config
+            pkgs.clang
+            # Mold Linker for faster builds (only on Linux)
+            (lib.optionals pkgs.stdenv.isLinux pkgs.mold)
+          ];
+          buildInputs = [
+            # We want the unwrapped version, wrapped comes with nixpkgs' toolchain
+            pkgs.rust-analyzer-unwrapped
+            # Finally the toolchain
+            toolchain
+          ];
+          packages = [
+            pkgs.cargo-nextest
+          ];
+          # Environment variables
+          RUST_SRC_PATH = "${toolchain}/lib/rustlib/src/rust/library";
+          # Needed for running DKG/Tangle locally
+          LD_LIBRARY_PATH = "${pkgs.gmp}/lib";
+        };
+      });
+}
