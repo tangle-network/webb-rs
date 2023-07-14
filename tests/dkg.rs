79a446278d647e4fdd49acf3da3cdd6b23f9f5e7
Shady Khalifa <dev+github@shadykhalifa.me>
2023-07-14 16:35:09 +0300

    fix the tests

--- tests/dkg.rs
+++ tests/dkg.rs
@@ -1,9 +1,8 @@
-use subxt::client::OfflineClientT;
 use subxt::config::PolkadotConfig;
 use subxt::tx:: PairSigner;
 use webb::substrate::tangle_runtime::api::dkg_proposals;
 use webb::substrate::tangle_runtime::api::runtime_types::bounded_collections::bounded_vec::BoundedVec;
-use webb::substrate::tangle_runtime::api::runtime_types::webb_proposals::header::{TypedChainId, ResourceId};
+use webb::substrate::tangle_runtime::api::runtime_types::webb_proposals::header::TypedChainId;
 use webb::substrate::tangle_runtime::api::runtime_types::webb_proposals::nonce::Nonce;
 use webb::substrate::tangle_runtime;
 use webb::substrate::tangle_runtime::api::runtime_types::webb_proposals::proposal::{Proposal, ProposalKind};
@@ -51,27 +50,15 @@ async fn read_chain_nonce() -> anyhow::Result<()> {
 async fn acknowledge_proposal_works() -> anyhow::Result<()> {
     let client = get_runtime_api().await?;
     let nonce = Nonce(0);
-    let src_id = TypedChainId::Evm(5001);
-    let r_id: [u8; 32] = hex::decode(
-        "0000000000000000e69a847cd5bc0c9480ada0b339d7f0a8cac2b6670000138a",
-    )?
-    .try_into()
-    .unwrap();
-    let r_id = ResourceId(r_id);
     let prop = hex::decode("0000000000000000e69a847cd5bc0c9480ada0b339d7f0a8cac2b6670000138a0000000000000000891300000000000003c951dfd2ab1e3e2864239ad09256b25ebadd164d53445c435bb31f036f3d36")?;
     let eve = sp_keyring::AccountKeyring::Eve;
     let signer = PairSigner::new(eve.pair());
     let acknowlege_proposal_tx = tangle_runtime::api::tx()
         .dkg_proposals()
-        .acknowledge_proposal(
-            nonce.clone(),
-            src_id,
-            r_id,
-            Proposal::Unsigned {
-                kind: ProposalKind::AnchorUpdate,
-                data: BoundedVec(prop),
-            },
-        );
+        .acknowledge_proposal(Proposal::Unsigned {
+            kind: ProposalKind::AnchorUpdate,
+            data: BoundedVec(prop),
+        });
     let result = client
         .tx()
         .sign_and_submit_then_watch_default(&acknowlege_proposal_tx, &signer)
