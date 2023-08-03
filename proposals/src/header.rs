a3bc0c15c1994ebdc556c10aab2c6eb28dd86f13
1xstj <106580853+1xstj@users.noreply.github.com>
2023-08-03 08:59:26 +0530

    derive serialize

--- proposals/src/header.rs
+++ proposals/src/header.rs
@@ -33,7 +33,7 @@ pub struct FunctionSignature(pub [u8; 4]);
         scale_codec::MaxEncodedLen
     )
 )]
-#[cfg_attr(feature = "std", derive(Serialize, Deserialize))]
+#[derive(Serialize, Deserialize)]
 pub struct ResourceId(pub [u8; 32]);
 
 /// Proposal Target Chain and its type (6 bytes).
