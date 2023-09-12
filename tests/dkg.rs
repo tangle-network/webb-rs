1045a9ed0ca042dc26e6e024bf14a18404e2c8c7
Shady Khalifa <dev+github@shadykhalifa.me>
2023-09-12 19:23:19 +0300

    Fix tests

--- tests/dkg.rs
+++ tests/dkg.rs
@@ -1,5 +1,4 @@
 use subxt::config::PolkadotConfig;
-use subxt::tx:: PairSigner;
 use webb::substrate::tangle_runtime::api::dkg_proposals;
 use webb::substrate::tangle_runtime::api::runtime_types::bounded_collections::bounded_vec::BoundedVec;
 use webb::substrate::tangle_runtime::api::runtime_types::webb_proposals::header::TypedChainId;
@@ -51,8 +50,8 @@ async fn acknowledge_proposal_works() -> anyhow::Result<()> {
     let client = get_runtime_api().await?;
     let nonce = Nonce(0);
     let prop = hex::decode("0000000000000000e69a847cd5bc0c9480ada0b339d7f0a8cac2b6670000138a0000000000000000891300000000000003c951dfd2ab1e3e2864239ad09256b25ebadd164d53445c435bb31f036f3d36")?;
-    let eve = sp_keyring::AccountKeyring::Eve;
-    let signer = PairSigner::new(eve.pair());
+    let eve = subxt_signer::sr25519::dev::eve();
+
     let acknowlege_proposal_tx = tangle_runtime::api::tx()
         .dkg_proposals()
         .acknowledge_proposal(Proposal::Unsigned {
@@ -61,7 +60,7 @@ async fn acknowledge_proposal_works() -> anyhow::Result<()> {
         });
     let result = client
         .tx()
-        .sign_and_submit_then_watch_default(&acknowlege_proposal_tx, &signer)
+        .sign_and_submit_then_watch_default(&acknowlege_proposal_tx, &eve)
         .await?
         .wait_for_finalized_success()
         .await?;
