178fe9fa4e7ca4f348dbfacb91a93010fb62212b
salman01zp <pathansalman555@gmail.com>
2024-03-01 12:35:51 +0530

    remove subxt metadata generation from webb-rs

--- README.md
+++ README.md
@@ -8,34 +8,6 @@
 
 <br />
 
-### Downloading metadata from a Substrate node
-
-Use the [`subxt-cli`](https://lib.rs/crates/subxt-cli) tool to download the metadata for your target runtime from a node.
-
-1. Install:
-```bash
-cargo install subxt-cli
-```
-
-2. To Save the metadata of `tangle`:
-Run the release build of the `tangle` node, then on another terminal run:
-
-```bash
-subxt metadata -f bytes > ./metadata/tangle-runtime.scale
-```
-Same goes for any future runtimes.
-
-> Note: This defaults to querying the metadata of a locally running node on the default `http://localhost:9933/`. If querying
-a different node then the `metadata` command accepts a `--url` argument.
-
-4. Generating the rust code from the metadata:
-
-```bash
-cargo build --features generate-substrate
-```
-
-> Tip: See the [build.rs](./build.rs) file to see how everything is being generated.
-
 
 ### Generating EVM Contracts
 
--- build.rs
+++ build.rs
@@ -1,8 +1,5 @@
 use std::error::Error;
 
-#[cfg(feature = "generate-substrate")]
-use subxt_codegen::TypeSubstitutes;
-
 #[cfg(feature = "generate-contracts")]
 mod evm {
     use super::*;
@@ -246,66 +243,6 @@ mod evm {
     }
 }
 
-#[cfg(feature = "generate-substrate")]
-mod substrate {
-    use super::*;
-    use scale::Decode;
-    use subxt_codegen::CratePath;
-
-    fn parse_and_generate_runtime(
-        path: &str,
-        out: &str,
-    ) -> Result<(), Box<dyn Error>> {
-        println!("cargo:rerun-if-changed=./{}", path);
-        let bytes = std::fs::read(path)?;
-
-        let metadata =
-            <subxt_metadata::Metadata as Decode>::decode(&mut &bytes[..])?;
-        let crate_path = CratePath::default();
-        // Module under which the API is generated.
-        let item_mod = syn::parse_quote!(
-            pub mod api {}
-        );
-        // Default type substitutes.
-        let substs = TypeSubstitutes::with_default_substitutes(&crate_path);
-        // Generate the Runtime API.
-        let generator = subxt_codegen::RuntimeGenerator::new(metadata);
-        let mut generated_type_derives =
-            subxt_codegen::DerivesRegistry::with_default_derives(&crate_path);
-
-        generated_type_derives.extend_for_all(
-            [
-                syn::parse_quote!(Eq),
-                syn::parse_quote!(PartialEq),
-                syn::parse_quote!(Clone),
-            ]
-            .into_iter(),
-            [],
-        );
-
-        // Include metadata documentation in the Runtime API.
-        let generate_docs = true;
-        let runtime_api = generator.generate_runtime(
-            item_mod,
-            generated_type_derives,
-            substs,
-            crate_path,
-            generate_docs,
-        )?;
-        let syntax_tree = syn::parse_file(&runtime_api.to_string()).unwrap();
-        let formatted = prettyplease::unparse(&syntax_tree);
-        std::fs::write(out, formatted)?;
-        Ok(())
-    }
-
-    pub fn generate_tangle_runtime() -> Result<(), Box<dyn Error>> {
-        parse_and_generate_runtime(
-            "metadata/tangle-runtime.scale",
-            "src/substrate/tangle_runtime.rs",
-        )
-    }
-}
-
 fn main() -> Result<(), Box<dyn Error>> {
     #[cfg(feature = "generate-contracts")]
     {
@@ -334,10 +271,6 @@ fn main() -> Result<(), Box<dyn Error>> {
         evm::build_protocol_solidity_masp_vanchor_batch_tree()?;
         evm::build_protocol_solidity_masp_vanchor_tree()?;
         evm::build_protocol_solidity_masp_vanchor()?;
-    }
-    #[cfg(feature = "generate-substrate")]
-    {
-        substrate::generate_tangle_runtime()?;
     }
     Ok(())
 }
